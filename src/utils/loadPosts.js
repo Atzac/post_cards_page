a1d475b7190cb14cec0be6f6ad2f667e7e95e907
isaac_quidute <isaacquidutemelo13@gmail.com>
2022-01-16 23:42:03 -0300

    full deploy

---
+++ src/utils/loadPosts.js
@@ -0,0 +1,15 @@
+export const loadPosts = async () => {
+    const postsTexts = fetch('https://jsonplaceholder.typicode.com/posts')
+    const postsPhotos = fetch("https://jsonplaceholder.typicode.com/photos")
+
+    const [posts, photos] = await Promise.all([postsTexts, postsPhotos])
+
+    const postsJsonParsed = await posts.json()
+    const photosJsonParsed = await photos.json()
+
+    const postsAndPhotos = postsJsonParsed.map((post, index) => {
+        return { ...post, cover: photosJsonParsed[index].url }
+    })
+    
+    return postsAndPhotos;
+}
